4782351bd7cb2ffb9b628e50c270df17823b1fc1
Amr Salah <amrs22131@gmail.com>
2022-08-12 19:51:49 +0200

    refactored code to use viewbinding

--- app/src/main/java/com/asu/calculator/MainActivity.java
+++ app/src/main/java/com/asu/calculator/MainActivity.java
@@ -11,13 +11,21 @@ import com.asu.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    TextView numbers;
+    static int operand=0;
+    static short operator=0;
+
     View.OnClickListener num_listener = new View.OnClickListener() {
         @Override
-        public void onClick(View ob1) {
-            Button tempBtn = (Button) ob1;
-            numbers.append(tempBtn.getText().toString());
-            double x = Double.parseDouble(numbers.getText().toString());
+        public void onClick(View displayText) {
+            Button tempBtn = (Button) displayText ;
+            binding.display.append(tempBtn.getText().toString());
+        }
+    };
+    View.OnClickListener operation_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View displayText) {
+            Button tempBtn = (Button) displayText ;
+            binding.display.append(tempBtn.getText().toString());
         }
     };
     @Override
@@ -26,6 +34,21 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-
+        binding.oneButton.setOnClickListener(num_listener);
+        binding.twoButton.setOnClickListener(num_listener);
+        binding.threeButton.setOnClickListener(num_listener);
+        binding.fourButton.setOnClickListener(num_listener);
+        binding.fiveButton.setOnClickListener(num_listener);
+        binding.sixButton.setOnClickListener(num_listener);
+        binding.sevenButton.setOnClickListener(num_listener);
+        binding.eightButton.setOnClickListener(num_listener);
+        binding.nineButton.setOnClickListener(num_listener);
+        binding.zeroButton.setOnClickListener(num_listener);
+        binding.doubleZerosButton.setOnClickListener(num_listener);
+        binding.addButton.setOnClickListener(operation_listener);
+        binding.subtractButton.setOnClickListener(operation_listener);
+        binding.multiplyButton.setOnClickListener(operation_listener);
+        binding.divisionButton.setOnClickListener(operation_listener);
+        binding.precentageButton.setOnClickListener(operation_listener);
     }
 }
