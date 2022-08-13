1b6f6e4d04a157d7bbbc6bb8fc2f28c8840302d3
Amr Salah <amrs22131@gmail.com>
2022-08-13 04:28:01 +0200

    finished basic functionalities

--- app/src/main/java/com/asu/calculator/MainActivity.java
+++ app/src/main/java/com/asu/calculator/MainActivity.java
@@ -2,74 +2,102 @@ package com.asu.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
 import com.asu.calculator.databinding.ActivityMainBinding;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    static int operand=0;
-    static short operator=0;
+    boolean lastNumeric = false;
+    Expression result ;
+
+
 
     View.OnClickListener num_listener = new View.OnClickListener() {
         @Override
         public void onClick(View displayText) {
             Button tempBtn = (Button) displayText ;
-            binding.display.append(tempBtn.getText().toString());
-        }
+            String buttonText = tempBtn.getText().toString();
+            binding.display.append(buttonText);
+            lastNumeric = true;
+            }
     };
+
     View.OnClickListener operation_listener = new View.OnClickListener() {
         @Override
         public void onClick(View displayText) {
             Button tempBtn = (Button) displayText ;
-
-            switch(operator){
-                case 0:
-                    operand+=Integer.parseInt(binding.display.getText().toString());
-                    break;
-                case 1:
-                    operand-=Integer.parseInt(binding.display.getText().toString());
-                    break;
-                case 2:
-                    operand*=Integer.parseInt(binding.display.getText().toString());
-                    break;
-                case 3:
-                    operand/=Integer.parseInt(binding.display.getText().toString());
-                    break;
-                case 4:
-                    binding.display.setText(Integer.toString(operand));
-                    break;
-            }
-            binding.display.setText(Integer.toString(operand));
-            if(tempBtn.getText().toString().equals("+")) operator=0;
-            if(tempBtn.getText().toString().equals("-")) operator=1;
-            if(tempBtn.getText().toString().equals("*")) operator=2;
-            if(tempBtn.getText().toString().equals("/")) operator=3;
-            if(tempBtn.getText().toString().equals("=")) operator=4;
-
-        }
-    };
-
-    View.OnClickListener hidden_operation_listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View displayText) {
-            Button tempBtn = (Button) displayText ;
+            String buttonText = tempBtn.getText().toString();
             String displayString = binding.display.getText().toString();
-            if(tempBtn.getText().toString().equals("C")) operator=0;
-            if(tempBtn.getText().toString().equals("\u232b")) operator=1;
-            switch(operator){
-                case 0:
+            switch (buttonText){
+                case "\u002b":
+                    if(lastNumeric){
+                        binding.display.append("+");
+                        lastNumeric = false ;
+                    }
+                    break;
+                case "\u2212":
+                    if(lastNumeric){
+                        binding.display.append("-");
+                        lastNumeric = false ;
+                    }
+                    break;
+                case "\u00D7":
+                    if(lastNumeric){
+                        binding.display.append("*");
+                        lastNumeric = false ;
+                    }
+                    break;
+                case "\u00F7":
+                    if(lastNumeric){
+                        binding.display.append("/");
+                        lastNumeric = false ;
+                    }
+                    break;
+                case "\u003D":
+                    if(lastNumeric){
+                        if(!displayString.contains("/0")){
+                            result = new ExpressionBuilder(displayString).build();
+                            binding.display.setText(String.valueOf(result.evaluate()));
+                        }
+                    }
+                    break;
+                case "\u002E":
+                    if(lastNumeric){
+                        binding.display.append(".");
+                        lastNumeric = false ;
+                    }
+                    break;
+                case "C":
                     binding.display.setText("");
+                    lastNumeric=false;
                     break;
-                case 1:
-                    binding.display.setText(displayString.substring(0,displayString.length()-1));
+                case"\u232b":
+                    if(!displayString.isEmpty())
+                        binding.display.setText(displayString.substring(0,displayString.length()-1));
+                    String newDisplay = binding.display.getText().toString();
+                    System.out.println(newDisplay);
+                    if(!(newDisplay.endsWith("-")||newDisplay.endsWith("+")||
+                            newDisplay.endsWith("*")||newDisplay.endsWith("/")))
+                        lastNumeric=true;
                     break;
+                case "+/-":
+                    if(displayString.startsWith("-"))
+                        binding.display.setText(displayString.replaceFirst("-",""));
+                    else
+                        binding.display.setText("-"+displayString);
             }
         }
     };
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,8 +120,10 @@ public class MainActivity extends AppCompatActivity {
         binding.subtractButton.setOnClickListener(operation_listener);
         binding.multiplyButton.setOnClickListener(operation_listener);
         binding.divisionButton.setOnClickListener(operation_listener);
-        binding.precentageButton.setOnClickListener(operation_listener);
-        binding.clearButton.setOnClickListener(hidden_operation_listener);
-        binding.backspaceButton.setOnClickListener(hidden_operation_listener);
+        binding.negativeButton.setOnClickListener(operation_listener);
+        binding.equalButton.setOnClickListener(operation_listener);
+        binding.clearButton.setOnClickListener(operation_listener);
+        binding.backspaceButton.setOnClickListener(operation_listener);
+        binding.decimalButton.setOnClickListener(operation_listener);
     }
 }
