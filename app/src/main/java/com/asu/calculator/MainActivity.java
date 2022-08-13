3b6fab526afd08410feb7fef7ed67086647f1af5
Amr Salah <amrs22131@gmail.com>
2022-08-13 20:46:02 +0200

    added undo button

--- app/src/main/java/com/asu/calculator/MainActivity.java
+++ app/src/main/java/com/asu/calculator/MainActivity.java
@@ -1,20 +1,22 @@
 package com.asu.calculator;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import com.asu.calculator.databinding.ActivityMainBinding;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     boolean lastNumeric = false; //flag to check if last user input was a number
     Expression result ;// solve mathematical expressions library
-    double memory = 0;
+    double memory = 0; // initialize memory
+    String lastFormula; // for undo button
 
 
     //numbers listener
@@ -37,60 +39,62 @@ public class MainActivity extends AppCompatActivity {
             Button tempBtn = (Button) displayText ;
             String buttonText = tempBtn.getText().toString();
             String displayString = binding.display.getText().toString();
-            switch (buttonText){
+            switch (buttonText) {
                 //plus operand
                 case "\u002b":
-                    if(lastNumeric){
+                    if (lastNumeric) {
                         binding.display.append("+");
-                        lastNumeric = false ;
+                        lastNumeric = false;
                     }
                     break;
 
                 //minus operand
                 case "\u2212":
                     //make sure cant add more than two minus
-                    if(!displayString.endsWith("--")) {
+                    if (!displayString.endsWith("--")) {
                         binding.display.append("-");
-                        lastNumeric = false ;
+                        lastNumeric = false;
                     }
                     break;
 
                 //multiplication operand
                 case "\u00D7":
-                    if(lastNumeric){
+                    if (lastNumeric) {
                         binding.display.append("*");
-                        lastNumeric = false ;
+                        lastNumeric = false;
                     }
                     break;
 
                 //division operand
                 case "\u00F7":
-                    if(lastNumeric){
+                    if (lastNumeric) {
                         binding.display.append("/");
-                        lastNumeric = false ;
+                        lastNumeric = false;
                     }
                     break;
 
                 //equal operand
                 case "\u003D":
                     //check if last input was a number not an operand
-                    if(lastNumeric){
+                    if (lastNumeric) {
                         //prevent app crash because of arithmetic exceptions
                         try {
                             //solve the mathematical expression
                             result = new ExpressionBuilder(displayString).build();
                             //display result on screen
                             binding.display.setText(String.valueOf(result.evaluate()));
-                        }catch (ArithmeticException e){}
+                            lastFormula = displayString;
+                        } catch (ArithmeticException e) {
+                        }
                     }
                     break;
 
                 //decimal operand
                 case "\u002E":
                     //check if last input was a number not an operand
-                    if(lastNumeric){
+                    if (lastNumeric) {
                         binding.display.append(".");
-                        lastNumeric = false ;
+                        lastNumeric = false;
                     }
                     break;
 
@@ -98,30 +102,30 @@ public class MainActivity extends AppCompatActivity {
                 case "C":
                     //clear all text
                     binding.display.setText("");
-                    lastNumeric=false;
+                    lastNumeric = false;
                     break;
 
                 //backspace operand
-                case"\u232b":
+                case "\u232b":
                     //checks that the display is not empty
-                    if(!displayString.isEmpty())
+                    if (!displayString.isEmpty())
                         //remove last character
-                        binding.display.setText(displayString.substring(0,displayString.length()-1));
+                        binding.display.setText(displayString.substring(0, displayString.length() - 1));
                     String newDisplay = binding.display.getText().toString();
                     //allow to write operands again after deleting operand
-                    if(!(newDisplay.endsWith("-")||newDisplay.endsWith("+")||
-                            newDisplay.endsWith("*")||newDisplay.endsWith("/")))
-                        lastNumeric=true;
+                    if (!(newDisplay.endsWith("-") || newDisplay.endsWith("+") ||
+                            newDisplay.endsWith("*") || newDisplay.endsWith("/")))
+                        lastNumeric = true;
                     break;
 
                 //negative operand
                 case "+/-":
                     //if the number is negative change it to positive
-                    if(displayString.startsWith("-"))
-                        binding.display.setText(displayString.replaceFirst("-",""));
-                    //else change the number to negative
+                    if (displayString.startsWith("-"))
+                        binding.display.setText(displayString.replaceFirst("-", ""));
+                        //else change the number to negative
                     else
-                        binding.display.setText("-"+displayString);
+                        binding.display.setText("-" + displayString);
                     break;
 
                 case "MR":
@@ -131,27 +135,36 @@ public class MainActivity extends AppCompatActivity {
                     memory = 0;
                     break;
                 case "M+":
-                    if(lastNumeric){
+                    if (lastNumeric) {
                         //checks if there is division by zero
                         try {
                             //solve the mathematical expression
                             result = new ExpressionBuilder(displayString).build();
                             memory += result.evaluate();
-                        }catch (ArithmeticException e){}
+                        } catch (ArithmeticException e) {
+                        }
                     }
                     break;
                 case "M-":
-                    if(lastNumeric){
+                    if (lastNumeric) {
                         //checks if there is division by zero
                         try {
                             //solve the mathematical expression
                             result = new ExpressionBuilder(displayString).build();
                             memory -= result.evaluate();
-                        }catch (ArithmeticException e){}
+                        } catch (ArithmeticException e) {
+                        }
                     }
                     break;
-
             }
+        }
+    };
+
+    View.OnClickListener lastOperation = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //display last formula calculated
+            binding.display.setText(lastFormula);
         }
     };
 
@@ -163,6 +176,7 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setTheme(R.style.Theme_Calculator);
         setContentView(view);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         binding.oneButton.setOnClickListener(num_listener);
         binding.twoButton.setOnClickListener(num_listener);
         binding.threeButton.setOnClickListener(num_listener);
@@ -187,23 +201,29 @@ public class MainActivity extends AppCompatActivity {
         binding.MRbutton.setOnClickListener(operation_listener);
         binding.mPlusButton.setOnClickListener(operation_listener);
         binding.mMinusButton.setOnClickListener(operation_listener);
+        binding.undoButton.setOnClickListener(lastOperation);
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
-        // Save display and last numeric state
+        // save what is on display
         savedInstanceState.putString("displayString" , binding.display.getText().toString());
+        //save last numeric flag state
         savedInstanceState.putBoolean("lastNumeric", lastNumeric);
+        //save last formula calculated
+        savedInstanceState.putString("last formula", lastFormula);
         super.onSaveInstanceState(savedInstanceState);
     }
 
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        // Restore display and last numeric state
+        // Restore what was on display
          String displayString = savedInstanceState.getString("displayString");
          binding.display.setText(displayString);
-         boolean l = savedInstanceState.getBoolean("lastNumeric");
-         lastNumeric = l;
+         //restore last numeric flag state
+        lastNumeric = savedInstanceState.getBoolean("lastNumeric");
+         //restore last formula calculated
+        lastFormula = savedInstanceState.getString("last formula");
     }
 }
