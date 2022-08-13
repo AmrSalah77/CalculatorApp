b0f47b5333125882ef25028a4e260dd84fbce76a
Amr Salah <amrs22131@gmail.com>
2022-08-13 14:32:45 +0200

    commented code

--- app/src/main/java/com/asu/calculator/MainActivity.java
+++ app/src/main/java/com/asu/calculator/MainActivity.java
@@ -1,33 +1,33 @@
 package com.asu.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import com.asu.calculator.databinding.ActivityMainBinding;
-
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    boolean lastNumeric = false;
-    Expression result ;
+    boolean lastNumeric = false; //flag to check if last user input was a number
+    Expression result ;// solve mathematical expressions library
 
 
-
+    //numbers listener
     View.OnClickListener num_listener = new View.OnClickListener() {
         @Override
         public void onClick(View displayText) {
             Button tempBtn = (Button) displayText ;
             String buttonText = tempBtn.getText().toString();
+            //display numbers on screen
             binding.display.append(buttonText);
+            //set flag to true
             lastNumeric = true;
             }
     };
 
+    //non-numbers listener
     View.OnClickListener operation_listener = new View.OnClickListener() {
         @Override
         public void onClick(View displayText) {
@@ -35,60 +35,88 @@ public class MainActivity extends AppCompatActivity {
             String buttonText = tempBtn.getText().toString();
             String displayString = binding.display.getText().toString();
             switch (buttonText){
+                //plus operand
                 case "\u002b":
                     if(lastNumeric){
                         binding.display.append("+");
                         lastNumeric = false ;
                     }
                     break;
+
+                //minus operand
                 case "\u2212":
-                    if(lastNumeric){
+                    //make sure cant add more than two minus
+                    if(!displayString.endsWith("--")) {
                         binding.display.append("-");
                         lastNumeric = false ;
                     }
                     break;
+
+                //multiplication operand
                 case "\u00D7":
                     if(lastNumeric){
                         binding.display.append("*");
                         lastNumeric = false ;
                     }
                     break;
+
+                //division operand
                 case "\u00F7":
                     if(lastNumeric){
                         binding.display.append("/");
                         lastNumeric = false ;
                     }
                     break;
+
+                //equal operand
                 case "\u003D":
+                    //check if last input was a number not an operand
                     if(lastNumeric){
+                        //checks if there is division by zero
                         if(!displayString.contains("/0")){
+                            //solve the mathematical expression
                             result = new ExpressionBuilder(displayString).build();
+                            //display result on screen
                             binding.display.setText(String.valueOf(result.evaluate()));
                         }
                     }
                     break;
+
+                //decimal operand
                 case "\u002E":
+                    //check if last input was a number not an operand
                     if(lastNumeric){
                         binding.display.append(".");
                         lastNumeric = false ;
                     }
                     break;
+
+                //clear operand
                 case "C":
+                    //clear all text
                     binding.display.setText("");
                     lastNumeric=false;
                     break;
+
+                //backspace operand
                 case"\u232b":
+                    //checks that the display is not empty
                     if(!displayString.isEmpty())
+                        //remove last character
                         binding.display.setText(displayString.substring(0,displayString.length()-1));
                     String newDisplay = binding.display.getText().toString();
-                    System.out.println(newDisplay);
+                    //allow to write operands again after deleting operand
                     if(!(newDisplay.endsWith("-")||newDisplay.endsWith("+")||
                             newDisplay.endsWith("*")||newDisplay.endsWith("/")))
                         lastNumeric=true;
                     break;
+
+                //negative operand
                 case "+/-":
+                    //if the number is negative change it to positive
                     if(displayString.startsWith("-"))
                         binding.display.setText(displayString.replaceFirst("-",""));
+                    //else change the number to negative
                     else
                         binding.display.setText("-"+displayString);
             }
